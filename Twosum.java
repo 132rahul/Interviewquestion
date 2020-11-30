9f6a2455c82a6b5e21e3f6782f88f21d7a3bfa1f
Rahul <30731690+132rahul@users.noreply.github.com>
2020-11-30 11:43:53 +0530

    Add files via upload

---
+++ Twosum.java
@@ -0,0 +1,35 @@
+//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target
+//        Example 1:
+//        Input: nums = [2,7,11,15], target = 9
+//        Output: [0,1]
+//        Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+//        Example 2:
+//        Input: nums = [3,2,4], target = 6
+//        Output: [1,2]
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Twosum {
+    public static void main(String args[]) {
+        int[] nums = {3, 4, 5, 2, 1};
+        int target = 6;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        System.out.println(map);
+        for (int i = 0; i < nums.length; i++)
+            System.out.print(" " + nums[i]);
+        System.out.println();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && (map.get(complement) != i)) {
+                System.out.println(i + " " + map.get(complement));
+                //i, map.get(complement);
+            }
+        }
+
+    }
+}
